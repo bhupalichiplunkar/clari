b04a5125be79273de5fef6129e1fa881da08e0f1
Bhupali Chiplunkar <bhupali.chiplunkar@gmail.com>
2024-03-20 23:17:32 +0530

    authentication service

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::API
     include ActionController::Cookies
-
+    skip_before_action :verify_authenticity_token, raise: false
+    
     def hello_world
         session[:count] = (session[:count] || 0) + 1
         render json: { count: session[:count] }
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resource :sessions, only: [:create]
+
+  resources :registrations, only: [:create]
+
+  delete :logout, to: "sessions#logout"
+
+  get :logged_in, to: "sessions#logged_in"
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
